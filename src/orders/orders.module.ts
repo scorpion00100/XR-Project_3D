ab2e5dbc6b3b9489590138091576c9341ecd9457
scorpion00100 <danbetheliryivuze@gmail.com>
2023-09-13 11:23:49 +0200

    update config

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { OrderRepository } from './order.repository';
+import { Order } from './order.entity'; // Assurez-vous que le chemin est correct
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderRepository])],
+  imports: [TypeOrmModule.forFeature([Order])],
   providers: [OrdersService],
   controllers: [OrdersController],
 })
