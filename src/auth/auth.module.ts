2a7c0d0d5d85bef0b6128dd5558fe58daa4d7fca
scorpion00100 <danbetheliryivuze@gmail.com>
2023-10-16 14:15:00 +0200

    update config module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,21 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Importez TypeOrmModule
+import { User } from '../users/user.entity'; // Importez l'entité User
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.register({
-      secret: 'process.env.JWT_SECRET', // Changez ceci pour une clé secrète réelle
-      signOptions: { expiresIn: '1h' }, // Réglez la durée de validité du jeton
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
     }),
+    TypeOrmModule.forFeature([User]), // Importez le UserRepository dans l'AuthModule
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, UsersService],
+  exports: [AuthService],
 })
 export class AuthModule {}
