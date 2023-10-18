fdf5f067614deead02e0e296a2d0b37e4c9f7cd8
scorpion00100 <danbetheliryivuze@gmail.com>
2023-10-18 17:33:10 +0200

    push

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,21 +1,5 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
-import { AuthService } from './auth.service';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
-    super();
-  }
-
-  async validate(email: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(email, password);
-
-    if (!user) {
-      throw new UnauthorizedException('Identifiants invalides');
-    }
-
-    return user;
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
