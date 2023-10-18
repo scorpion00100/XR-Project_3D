7fcaca80fe8b757d9435b59a0482c4d933083af5
scorpion00100 <danbetheliryivuze@gmail.com>
2023-10-18 17:00:49 +0200

    hachage mot de passe

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,26 +2,24 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt'; // Importation de bcrypt
+import * as bcrypt from 'bcrypt';
 import { User } from '../users/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
-    @InjectRepository(User) // Injectez le repository ici
-    private readonly userRepository: Repository<User>, // Propriété du repository
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async validateUser(email: string, password: string) {
-    // Recherche de l'utilisateur par email
     const user = await this.findUserByEmail(email);
 
     if (!user) {
       throw new UnauthorizedException('Utilisateur non trouvé');
     }
 
-    // Comparaison du mot de passe fourni avec le mot de passe haché enregistré
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
@@ -32,20 +30,19 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
   }
 
   async createToken(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { email: user.email, sub: user.id };
     const token = this.jwtService.sign(payload);
     return token;
   }
 
   private async findUserByEmail(email: string): Promise<User | null> {
-    // Recherche de l'utilisateur par email dans la base de données
     return this.userRepository.findOne({ where: { email } });
   }
 }
